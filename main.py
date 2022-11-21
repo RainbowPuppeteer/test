e9084ec2af20e1a702f35ce2848f1a7082183718
RainbowPuppeteer <71234878+RainbowPuppeteer@users.noreply.github.com>
2022-11-20 20:20:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('remote')
+    print_hi('remote 2')
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
